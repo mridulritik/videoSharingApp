87d51a8a98d45158250ecb56c0c91d1a9457eb64
mridulritik <mridultiwari522@gmail.com>
2025-04-11 21:09:04 +0530

    setup of express

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 import DB_NAME from "../constant.js";
 
-const connectDb = async () =>{
+const connectDb = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`mongodb host || db connected: ${connectionInstance.connection.host}`)
     } catch (error) {
-        console.log(`ERROR:(error in db/index.js file while connecting)`,error)
+        console.log(`ERROR:(error in db/index.js file while connecting)`, error)
         process.exit(1)
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import dotenv from "dotenv"
 import connectDb from "./db/index.js "
+import { app } from "./app.js"
 
 
 dotenv.config({
-    path:".env"
+    path: ".env"
 })
 
-try {
-    connectDb() 
-    console.log(`dbconnected`)
-} catch (error) {
-    console.log(error);
-    
-} 
+
+connectDb()
+.then(()=>{
+    app.on("error",(err)=>{
+        console.log(err)
+    })
+    app.listen(process.env.PORT|| 5000,()=>{
+        console.log(`app is listening on port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log(`error occur while execution of connectDB function in index.js file: ${error}`)
+
+})
+
+
